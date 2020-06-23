d1c153a461a0eec016660c63db0f2c42f9618c23
Ekaterina Avdeeva <katavdeeva@gmail.com>
2020-06-23 12:08:30 -0600

    update 06.23.2020

---
+++ test/specs/clarinsusa.js
@@ -0,0 +1,11 @@
+// describe('Clarins Modal Window', () => {
+//     before(()=>{
+//         browser.maximizeWindow();
+//         browser.deleteAllCookies();
+//         browser.url('https://www.clarinsusa.com/en/skincare/skincare/');
+//
+//     })
+//     it('should verify that the modal window pops up', function () {
+//
+//     });
+// })
